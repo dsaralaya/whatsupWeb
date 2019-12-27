de8fe296a765accc26af32e539f52d84075d1233
deepaks <deepaks@novigosolutions.com>
2019-12-27 21:11:21 +0530

    settings

--- server/src/api/Settings/settings.contoller.ts
+++ server/src/api/Settings/settings.contoller.ts
@@ -1,24 +1,22 @@
 import { Request, Response } from "express";
-import * as fs from 'fs-extra';
-var dir = require('path').join(__dirname, '../cred.json');
+var fs = require('fs');
+var dir = require('path').join(__dirname, '../../cred.json');
 
 export default class SettingsController {
 
     public async update(req: Request, res: Response) {
-        fs.writeFile(dir, req.body, (err) => {
-            return ({ status: "success", statusCode: "200", data: req.body });
+        fs.writeFile(dir, JSON.stringify(req.body), (err) => {
+            return res.send({ status: "success", statusCode: "200", data: req.body });
         });
-       
+
     }
 
-
     public async get(req: Request, res: Response) {
-     
-        await fs.readFile(dir, { encoding: 'utf-8' },(err,data) => {
-            console.log(err);
-            console.log(data);
-            return ({ status: "success", statusCode: "200", data: data });
+
+        await fs.readFile(dir, { encoding: 'utf-8' }, (err, data) => {
+
+            return res.send({ status: "success", statusCode: "200", data: JSON.parse(data) });
         });
-       
+
     }
 }
